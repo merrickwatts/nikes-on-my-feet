76ff7f6205884e5869e24b73bea636fa1685fedf
Jayden Cowley <jcowley16@gmail.com>
2022-08-10 20:32:03 -0600

    creating homeContainer.js

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -25,3 +25,4 @@ function Nav({ currentPage, handlePageChange }) {
         </ul>
     )
 }
+export default Nav
--- client/src/components/ProductView.js
+++ client/src/components/ProductView.js
@@ -34,3 +34,4 @@ function ProductView({ currentPage, handlePageChange }) {
     </div>
   );
 }
+export default ProductView;
