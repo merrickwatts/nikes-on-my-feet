83c17b8be02360065af930574fb67660d1d010cf
Jayden Cowley <jcowley16@gmail.com>
2022-08-13 10:55:02 -0600

    adding ProductCard.js

---
+++ client/public/src/components/ProductCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function ProductCard(props) {
+  return (
+    <div className="container">
+      <div className="card">
+        <img></img>
+        <div className="card-body">
+          <h5 className="card-title">Name: {props.name}</h5>
+          <p className="card-text">Description: {props.description}</p>
+          <p className="card-text">Size: {props.size}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
