5b86e3e0eef2b48e7479a45479baa81a2522d902
Jennifer <goodwije@gmail.com>
2022-08-13 13:47:40 -0600

    moving files and folders for cohesion

--- client/public/src/index.js
+++ client/public/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from '../../src/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import * as React from 'react';
 
 // 1. import `ChakraProvider` component
