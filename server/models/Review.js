9de4785cee84ffa0324296cefc7aa921b07daf5a
Merrick Watts <merricklwatts@gmail.com>
2022-08-15 20:58:38 -0600

    adding some files to the server as well as seeds

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,11 +1,11 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const reviewSchema = new Schema(
   {
     reviewText: {
       type: String,
-      required: 'You need to leave a thought!',
+      required: "You need to leave a thought!",
       minlength: 1,
       maxlength: 280,
     },
@@ -15,12 +15,16 @@ const reviewSchema = new Schema(
       get: (timestamp) => dateFormat(timestamp),
     },
     username: {
-      type: String,
-      required: true,
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     user_id: {
       type: String,
       required: true,
+    },
+    product_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Shoe",
     },
   },
   {
@@ -30,6 +34,6 @@ const reviewSchema = new Schema(
   }
 );
 
-const Review = model('Review', reviewSchema);
+const Review = model("Review", reviewSchema);
 
 module.exports = Review;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,28 +1,31 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Review, Shoe } = require('../models');
-const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Review, Shoe } = require("../models");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id }).select(
-          '-__v -password'
+          "-__v -password"
         );
 
         return userData;
       }
 
-      throw new AuthenticationError('Not logged in');
+      throw new AuthenticationError("Not logged in");
     },
     users: async () => {
-      return User.find().select('-__v -password');
+      return User.find().select("-__v -password");
     },
     user: async (parent, { username }) => {
-      return User.findOne({ username }).select('-__v -password');
+      return User.findOne({ username }).select("-__v -password");
     },
     shoes: async () => {
       return Shoe.find();
+    },
+    reviews: async (parent) => {
+      return Review.find();
     },
   },
 
@@ -37,13 +40,13 @@ const resolvers = {
       const user = await User.findOne({ email });
 
       if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
+        throw new AuthenticationError("Incorrect credentials");
       }
 
       const token = signToken(user);
